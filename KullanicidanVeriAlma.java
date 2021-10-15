f093e2a5698c2b79c2df36caa5200d4a6e6ee679
Umut Haydar <89686179+umuthaydar@users.noreply.github.com>
2021-10-15 19:50:52 +0300

    Kullanıcıdan Veri Alma

---
+++ KullanicidanVeriAlma.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class KullanicidanVeriAlma {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lütfen isminizi giriniz: ");
+        String isim = scanner.nextLine();
+        System.out.println("İsminiz: " + isim );
+    }
+}
